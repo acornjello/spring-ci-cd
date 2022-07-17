876240f0fd61ee234156cc65453a1765b432a303
acornjello <acornjello14@gmail.com>
2022-07-17 13:52:05 +0900

    test HelloController("/hello/dto")

--- src/main/java/com/example/springcicd/web/HelloController.java
+++ src/main/java/com/example/springcicd/web/HelloController.java
@@ -1,12 +1,10 @@
 package com.example.springcicd.web;
 
+import com.example.springcicd.dto.HelloResponseDto;
 import com.example.springcicd.repository.PostsRepository;
 import com.example.springcicd.dto.PostsSaveRequestDto;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -17,6 +15,11 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 
     @PostMapping("/posts")
--- src/test/java/com/example/springcicd/web/HelloControllerTest.java
+++ src/test/java/com/example/springcicd/web/HelloControllerTest.java
@@ -1,14 +1,15 @@
 package com.example.springcicd.web;
 
+import com.example.springcicd.dto.HelloResponseDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -26,7 +27,22 @@ public class HelloControllerTest {
         mvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(hello));
+    }
 
+    @Test
+    public void helloDto가_리턴된다() throws Exception {
+        // given
+        String name = "hello";
+        int amount = 1000;
+        HelloResponseDto helloResponseDto = new HelloResponseDto(name, amount);
+
+        // when, then
+        mvc.perform(get("/hello/dto")
+                .param("name", name)
+                .param("amount", String.valueOf(amount)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is(name)))
+                .andExpect(jsonPath("$.amount", is(amount)));
     }
 
 
